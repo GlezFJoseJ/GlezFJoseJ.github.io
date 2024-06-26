054e8bf063d9a5125a4e00d6cfa7919e43821059
José Juan González Fuentes <killexcrilex@gmail.com>
2024-06-26 17:28:27 -0600

    Animations
    Animaciones de deslizamiento y agrandar

--- Recursos/js/cont.js
+++ Recursos/js/cont.js
@@ -10,3 +10,20 @@ function resizeImage() {
     window.addEventListener('load', resizeImage);
     window.addEventListener('resize', resizeImage);
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav a');
+
+    links.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
